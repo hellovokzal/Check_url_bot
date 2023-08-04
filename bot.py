27950f08578a31972a75dc8700b34aeca3127dc4
hellovokzal <100135590+hellovokzal@users.noreply.github.com>
2023-08-04 15:13:42 +0500

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,25 @@
+import requests as req
+import telebot
+import threading as t
+
+def check_url():
+    global url
+    try:
+        code = req.get(url)
+        bot.send_message(message.chat.id, f"Url: `{url}`\nStatus code: `{code.status_code}`")
+    except:
+        bot.send_message(message.chat.id, f"Url: `{url}`\nStatus code: `403 Forbidden`")
+    
+bot = telebot.Telebot("6506417857:AAGo4jRMB4obCIUgzSZo82n1SsWLyGnqL20")
+bot.message_handler(commands=['/start'])
+def start(message):
+    bot.send_message(message.chat.id, "Пиши команду /check <ссылка>\n/check https://google.com")
+bot.message_handler(commands=['check'])
+def check(mesaage):
+    url = message.text[7:len(message.text)]
+    if url[0:8] == "https://" or url[0:7] == "http://":
+        check_host = t.Thread(target=check_url)
+        check_host.start()
+    else:
+        bot.send_message(message.chat.id, f"Url: `{url}`\nStatus code: `Error target url!`")
+bot.polling(none_stop=True)
