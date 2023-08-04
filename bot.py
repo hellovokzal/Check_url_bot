ce1d3fddeba8ae28b16ca27b707e03013543c7c2
hellovokzal <100135590+hellovokzal@users.noreply.github.com>
2023-08-04 15:32:41 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 import requests as req
 import telebot
 import threading as t
-bot = telebot.TeleBot("6506417857:AAGo4jRMB4obCIUgzSZo82n1SsWLyGnqL20")
 
 def check_url():
     global url
@@ -11,10 +10,11 @@ def check_url():
     except:
         bot.send_message(message.chat.id, f"Url: `{url}`\nStatus code: `403 Forbidden`")
     
-bot.message_handler(commands=['start'])
+bot = telebot.Telebot("6506417857:AAGo4jRMB4obCIUgzSZo82n1SsWLyGnqL20")
+@bot.message_handler(commands=['/start'])
 def start(message):
     bot.send_message(message.chat.id, "Пиши команду /check <ссылка>\n/check https://google.com")
-bot.message_handler(commands=['check'])
+@bot.message_handler(commands=['check'])
 def check(mesaage):
     url = message.text[7:len(message.text)]
     if url[0:8] == "https://" or url[0:7] == "http://":
